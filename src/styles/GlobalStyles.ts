26237f439e87a366050b9ead556c41b841ba3805
Samuel Cupertino <sacupertinno2014@gmail.com>
2022-03-22 00:43:05 -0300

    local font static support

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,14 +1,24 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'himagsikan';
+    src: url('/static/fonts/himagsikan.woff');
+  }
+  @font-face {
+    font-family: 'Azonix';
+    src: url('/static/fonts/Azonix.woff');
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Azonix', monospace, sans-serif;
   }
 
-  body {
-    font-family: 'Roboto', sans-serif;
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'himagsikan', monospace, sans-serif;
   }
 
   #__next {
