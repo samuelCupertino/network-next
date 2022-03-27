501361c6d1d59772b25ba5d8378f549eec27f20a
Samuel Cupertino <sacupertinno2014@gmail.com>
2022-03-27 19:57:08 -0300

    hotfix styled component console error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', 'cdn.jsdelivr.net']
+  },
+
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true
   }
 }
 
